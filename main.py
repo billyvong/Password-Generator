66bebd3da9e90093ca5ded6813189b0847513ed9
bvongdara <96890287+billyvong@users.noreply.github.com>
2022-01-01 05:59:04 +0000

    Converted list into string

--- main.py
+++ main.py
@@ -11,16 +11,27 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 #Eazy Level - Order not randomised:
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
-
 for n in range(0,nr_letters):
   password = random.choices(letters,k=nr_letters)
 for n in range(0,nr_symbols):
   password.append(random.choice(symbols))
 for n in range(0,nr_numbers):
   password.append(random.choice(numbers))
-print(''.join(password))
+''.join(password)
+
+#Convert simple password list into string
+simp_pass = ""
+for char in password:
+  simp_pass += char
+print(f"Your simple password is: {simp_pass}")
 
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
-random_pass =(random.sample(password,len(password)))
-print(''.join(random_pass))
+random_pass = (random.sample(password,len(password)))
+''.join(random_pass)
+
+#Convert random password list into string
+hard_pass = ""
+for char in random_pass:
+  hard_pass += char
+print(f"Your strong, random password is: {hard_pass}")
